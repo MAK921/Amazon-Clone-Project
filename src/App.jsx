d2cc7815ad86b23077775ca7a377034b28ffad2e
MAK921 <abera9@yahoo.com>
2025-03-11 00:22:17 +0300

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,5 @@
-// import './App.css';
-// import Routing from './Router';
-// import Footer from './Components/Footer/Footer';
-// import { useContext, useEffect } from 'react';
-// import { DataContext } from './Components/DataProvider/DataProvider';
-// import { auth } from './Utility/firebase';
-// import {Type} from './Utility/action.type'
-
-
-// function App() {
-//   const [{ user }, dispatch] = useContext(DataContext);
-
-//   useEffect(() => {
-//     const unsubscribe = auth.onAuthStateChanged((authUser) => {
-//       if (authUser) {
-//         // console.log(authUser);
-//         dispatch({
-//         type:Type.SET_USER,
-//         user:authUser
-//         })
-//       }else{
-//        dispatch({
-//         type:Type.SET_USER,
-//         user:null,
-//         })
-//       }
-//     });
-//     return () => {
-//       unsubscribe();
-//     };
-//   }, []);
-
-//   return (
-//     <>
-//       <Routing />
-//       <Footer />
-//     </>
-//   );
-// }
-
-// export default App;
-
-
-
 import './App.css';
 import Routing from './Router';
-import Footer from './Components/Footer/Footer';
 import { useContext, useEffect } from 'react';
 import { DataContext } from './Components/DataProvider/DataProvider';
 import { auth } from './Utility/firebase';
@@ -75,7 +30,6 @@ function App() {
   return (
     <>
       <Routing />
-      <Footer />
     </>
   );
 }
--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Carousel from '../../Components/Carousel/Carousel'
 import Category from '../../Components/Category/Category'
 import Product from '../../Components/Product/Product'
-import Layout from '../../Components/Layout/Layout'
+import Footer from '../../Components/Footer/Footer'
 function Landing() {
   return (
     <>
       <Carousel />
       <Category />
       <Product />
+      <Footer />
       
     </>
   )
