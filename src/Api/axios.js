85050a55927d323389b89daa3a5535ed50328d22
MAK921 <abera9@yahoo.com>
2025-03-11 05:44:37 +0300

    last commit

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-6dc63/us-central1/api",
-  //   baseURL: "https://amazon-backend-gfvo.onrender.com",
+  //   baseURL: "http://127.0.0.1:5001/clone-6dc63/us-central1/api",
+  baseURL: "https://amazon-backend-gfvo.onrender.com",
 });
 
 export { axiosInstance };
