ad8a66062db08fe621c8d5e5b6f67466850a553f
MAK921 <abera9@yahoo.com>
2025-03-12 00:50:06 +0300

    last commit

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,9 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vite.dev/config/
-
-// export default defineConfig({
-//   //  base: "/Amazon-Clone-Project/",
-//   plugins: [react()],
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
+
 export default defineConfig({
+  //  base: "/Amazon-Clone-Project/",
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1000, // Increase the warning limit to 1000 KB
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("lodash")) {
-              return "lodash"; // Separate lodash into its own chunk
-            }
-            if (id.includes("react")) {
-              return "react-vendor"; // Separate React libraries
-            }
-            return "vendor"; // General chunk for other large dependencies
-          }
-        },
-      },
-    },
-    // minify: "terser", // Better minification
-    terserOptions: {
-      compress: {
-        drop_console: true, // Remove console logs
-        drop_debugger: true,
-      },
-    },
-  },
-  optimizeDeps: {
-    include: ["lodash-es"], // Use tree-shakable lodash-es instead of full lodash
-  },
 });
