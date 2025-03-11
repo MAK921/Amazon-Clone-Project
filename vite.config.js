f1a9f7dbdef8dbc88c37ecdd40cdd34d714b3c33
MAK921 <abera9@yahoo.com>
2025-03-11 23:56:01 +0300

    last commit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,33 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+
+// export default defineConfig({
+//   // base: "/Amaz/on-Clone-Project/",
+//   plugins: [react()],
+// ({
+// 	manualChunks: {
+// 		lodash: ['lodash']
+// 	,}
+// });
+
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-
 export default defineConfig({
   // base: "/Amaz/on-Clone-Project/",
   plugins: [react()],
-
- ({
-	manualChunks: {
-		lodash: ['lodash']
-	}
-});
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          lodash: ["lodash"],
+        },
+      },
+    },
+  },
 });
