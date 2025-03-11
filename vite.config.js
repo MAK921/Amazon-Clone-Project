2810cca32ce3c34b355f145dee6cb879973c3e0e
MAK921 <abera9@yahoo.com>
2025-03-11 23:52:08 +0300

    last commit

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 
 export default defineConfig({
-  //  base: "/Amazon-Clone-Project/",
+  // base: "/Amaz/on-Clone-Project/",
   plugins: [react()],
-  
-  d: {
-    chunkSizeWarningLimit: 1000,
-  },
+
+ ({
+	manualChunks: {
+		lodash: ['lodash']
+	}
+});
 });
